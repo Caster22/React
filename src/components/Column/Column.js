e71a39faad9a5a0f0e364cc3cb3176ff2fb63b72
Michał Kopacz <kopaczmichal093@gmail.com>
2020-06-22 19:29:42 +0200

    Add columnRedux.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,7 +3,7 @@ import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
 //import Creator from '../Creator/Creator';
-//import {settings} from '../../data/dataStore';
+import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 
 
@@ -13,31 +13,35 @@ class Column extends React.Component {
       title: PropTypes.string,
     }
 
-    render() {
-      // eslint-disable-next-line react/prop-types
-      const {title, icon, cards} = this.props;
-      return (
-        <section className={styles.component}>
-          <h3 className={styles.title}>
-            {title}
-            <span className={styles.icon}>
-              <Icon name={icon} />
-            </span>
-          </h3>
-          <div>
-            {/* eslint-disable-next-line react/prop-types */}
-            {cards.map(cardData => (
-              <Card key={cardData.id} {...cardData} />
-            ))}
-          </div>
-          {/*
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
+
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const {title, icon, cards} = this.props;
+    return (
+      <section className={styles.component}>
+        <h3 className={styles.title}>
+          {title}
+          <span className={styles.icon}>
+            <Icon name={icon} />
+          </span>
+        </h3>
+        <div>
+          {/* eslint-disable-next-line react/prop-types */}
+          {cards.map(cardData => (
+            <Card key={cardData.id} {...cardData} />
+          ))}
+        </div>
+        {/*
           <div>
             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
           </div>
           */}
-        </section>
-      );
-    }
+      </section>
+    );
+  }
 }
 
 export default Column;
