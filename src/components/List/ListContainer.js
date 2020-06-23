4004c01a79b951cc02e8e379b07be5946f5b850d
Michał Kopacz <kopaczmichal093@gmail.com>
2020-06-23 21:10:13 +0200

    Fix bug in directory for columnRedux

--- src/components/List/ListContainer.js
+++ src/components/List/ListContainer.js
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux';
 import List from './List';
-import {getColumnsForList, createActionAddColumn} from 'src/redux/columnRedux';
+import {getColumnsForList, createActionAddColumn} from '../../redux/columnRedux';
 
 const mapStateToProps = (state, props) => ({
   columns: getColumnsForList(state, props.id),
