d20aa2ae1d0e90306f872a43bb6ce4d8bcc3ad60
Michał Kopacz <kopaczmichal093@gmail.com>
2020-06-25 15:54:19 +0200

    Fix typho

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './app.scss';
+import styles from './App.scss';
 import List from '../List/ListContainer';
 import PropTypes from 'prop-types';
 //import {listData} from '../../data/dataStore';
